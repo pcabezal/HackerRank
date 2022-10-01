6f63b5e1e167a411a0c2e29fbff9a5237cff34bf
Paul Cabezal <paulcabezal@gmail.com>
2022-10-01 11:04:37 -0400

    1 easy

--- easy-grid-challenge.js
+++ easy-grid-challenge.js
@@ -1,20 +1,14 @@
 function gridChallenge(grid) {
-
   let cur = [];
   // alphabetize input strings
   grid.forEach((e,i) => {
       let text = e.split('').sort();
       cur.push(text);
   })
-  console.log(cur);
-  
-  // iterate through each string to see if columns are also in alphabetized
-  // change answer to 'NO' if not
 
   let len = cur.length;
   for(let i = 0; i < len - 1; i++) {
-      for (let j = 0; j < len -1; j++) {
-        console.log(cur[i][j], cur[i +1][j]);
+      for (let j = 0; j < len - 1; j++) {
           if (cur[i][j] > cur[i+1][j]) {
             return 'NO';
           }
